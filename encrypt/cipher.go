9e7fff900411b251af5e1bb79ec41b99129d2e2c
Varun Gupta <varun.gupta3@honeywell.com>
2020-05-17 23:24:00 +0530

    Added encrypt and decrypt code logic into a package

---
+++ encrypt/cipher.go
@@ -0,0 +1,66 @@
+package encrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+)
+
+// Encrypt will take in a key and plaintext and return a
+// hexed representation of the encrypted value.
+// This code is based on the standard library from godoc.
+func Encrypt(key, plaintext string) (string, error) {
+	block, err := newCipherBlock(key)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
+
+	return fmt.Sprintf("%x", ciphertext), nil
+}
+
+//Decrypt will take in a key and a cipherHex (hexed representation
+// of the cipherText) and decrypt it.
+// This code is based on the standard library examples at godoc.
+func Decrypt(key, cipherHex string) (string, error) {
+	block, err := newCipherBlock(key)
+	if err != nil {
+		return "", err
+	}
+
+	cipherText, err := hex.DecodeString(cipherHex)
+	if err != nil {
+		return "", err
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		return "", errors.New("ciphertext too short")
+	}
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	stream.XORKeyStream(cipherText, cipherText)
+	return string(cipherText), nil
+}
+
+func newCipherBlock(key string) (cipher.Block, error) {
+	hasher := md5.New()
+	fmt.Fprint(hasher, key)
+	cipherKey := hasher.Sum(nil)
+	return aes.NewCipher(cipherKey)
+}
